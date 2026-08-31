652f7ece52313ce361a414cf671a1f7348861f05
ReinierNoob <reinierlammerts@hotmail.com>
2026-08-31 13:59:46 +0200

    Add Module 8 state endpoint

---
+++ app/api/adaptive/solution-architecture-module-8/state/route.ts
@@ -0,0 +1,4 @@
+import { createAdaptiveStateHandler } from "../../../../../lib/adaptive-module-route-factory";
+import { solutionArchitectureModule8Runtime } from "../../../../../lib/solution-architecture-module-8-runtime";
+
+export const GET = createAdaptiveStateHandler(solutionArchitectureModule8Runtime);
