b6445c38f77d2efae482ae47725bc5a5a5c51271
ReinierNoob <reinierlammerts@hotmail.com>
2026-08-31 14:00:06 +0200

    Add Module 9 diagnose endpoint

---
+++ app/api/adaptive/solution-architecture-module-9/diagnose/route.ts
@@ -0,0 +1,4 @@
+import { createAdaptiveDiagnoseHandler } from "../../../../../lib/adaptive-module-route-factory";
+import { solutionArchitectureModule9Runtime } from "../../../../../lib/solution-architecture-module-9-runtime";
+
+export const POST = createAdaptiveDiagnoseHandler(solutionArchitectureModule9Runtime);
