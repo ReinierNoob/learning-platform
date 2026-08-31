f9751db6f3edacd051b95f37dea57e440dfff7b8
ReinierNoob <reinierlammerts@hotmail.com>
2026-08-31 14:00:17 +0200

    Add Module 9 observe endpoint

---
+++ app/api/adaptive/solution-architecture-module-9/observe/route.ts
@@ -0,0 +1,4 @@
+import { createAdaptiveObserveHandler } from "../../../../../lib/adaptive-module-route-factory";
+import { solutionArchitectureModule9Runtime } from "../../../../../lib/solution-architecture-module-9-runtime";
+
+export const POST = createAdaptiveObserveHandler(solutionArchitectureModule9Runtime);
