a285ea732ebe43100854eea4222836ca268d1a0a
ReinierNoob <reinierlammerts@hotmail.com>
2026-08-31 13:59:24 +0200

    Add Module 8 observe endpoint

---
+++ app/api/adaptive/solution-architecture-module-8/observe/route.ts
@@ -0,0 +1,4 @@
+import { createAdaptiveObserveHandler } from "../../../../../lib/adaptive-module-route-factory";
+import { solutionArchitectureModule8Runtime } from "../../../../../lib/solution-architecture-module-8-runtime";
+
+export const POST = createAdaptiveObserveHandler(solutionArchitectureModule8Runtime);
